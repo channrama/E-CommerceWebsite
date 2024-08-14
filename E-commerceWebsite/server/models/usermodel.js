d1250a8ff555855771972802ad4f264cb66686c9
channrama <dschannappa93@gmail.com>
2024-08-15 00:26:46 +0530

    created user model

---
+++ E-commerceWebsite/server/models/usermodel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose"
+const userschema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    phone:{
+    type:string,
+    required:true
+    },
+    address:{
+        type:string,
+        required:true,
+    },
+    role:{
+        type:Number,
+        default:0
+
+    },
+
+
+},{timestamps:true});
+export default mongoose.model('users',userschema)
